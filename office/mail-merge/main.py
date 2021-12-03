db4bd0e26268b581edc3ef1f7d08cea04f0a33af
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-03 07:44:00 -0600

    working mail-merge

--- office/mail-merge/main.py
+++ office/mail-merge/main.py
@@ -6,3 +6,17 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("Input/Names/invited_names.txt", 'r') as namefile:
+    names = namefile.readlines()
+
+for i in range(len(names)):
+    names[i] = names[i].strip()
+
+with open("Input/Letters/starting_letter.txt", 'r') as letterfile:
+    source_text = letterfile.read()
+
+for name in names:
+    output_text = source_text.replace("[name]", name)
+    with open(f"Output/ReadyToSend/{name}.txt", 'w') as outfile:
+        outfile.write(output_text)
