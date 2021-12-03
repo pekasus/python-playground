663008cba50536ffeaf69bc732e19fa06fd2c6d0
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-03 06:36:58 -0600

    Update Snake Game to include persistant high score

--- games/snake-game/scoreboard.py
+++ games/snake-game/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", 'r') as f:
+            self.high_score = int(f.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,11 +17,20 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", 'w') as f:
+                f.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
