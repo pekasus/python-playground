ea23a15b39eed4e60baeacb430ed2fb9f4b2b083
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-04 23:17:10 -0600

    complete Pomodoro app

--- office/tkinter-gui/pomodoro/main.py
+++ office/tkinter-gui/pomodoro/main.py
@@ -14,12 +14,19 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 work_call = False
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
     global reps
@@ -43,23 +50,30 @@ def start_timer():
         countdown(long_break_sec)
         title_label.config(text="Break", fg=RED)
         playsound("sound/long_break.mp3")
+        reps = 0
     else:
         countdown(short_break_sec)
         title_label.config(text="Break", fg=PINK)
         playsound("sound/short_break.mp3")
 
 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
 def countdown(count):
+    global timer
     count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec < 10:
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, countdown, count - 1)
+        timer = window.after(1000, countdown, count - 1)
     else:
         start_timer()
-
+        marks = ""
+        for _ in range(math.floor(reps/2)):
+            marks += "✅"
+        check_marks.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -82,10 +96,10 @@ canvas.grid(column=1, row=1)
 start_button = tk.Button(text="Start", fg=YELLOW, highlightthickness=0, bd=0, borderwidth=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = tk.Button(text="Reset", fg=YELLOW, highlightthickness=0, bd=0)# , command=calculate)
+reset_button = tk.Button(text="Reset", fg=YELLOW, highlightthickness=0, bd=0, command=reset_timer)
 reset_button.grid(column=3, row=2)
 
-check_marks = tk.Label(text="✅", fg=GREEN, bg=YELLOW)
+check_marks = tk.Label(text="", fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 window.mainloop()
