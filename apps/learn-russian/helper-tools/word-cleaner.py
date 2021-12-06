3932bd70ac14d813e5f5c82a39bf2eb85ce28cad
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-06 09:47:53 -0600

    initialize learn-russian app

---
+++ apps/learn-russian/helper-tools/word-cleaner.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('1000-russian-words-raw.txt', delimiter=' ')
+df.drop(df.columns[1], axis=1, inplace=True)
+df.to_csv("1000-russian-words-cleaned.csv", index=False, encoding="UTF-8")
