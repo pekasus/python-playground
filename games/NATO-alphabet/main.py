5770a5db7914fa82d5b60dee5d18c06dbd64ccd0
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-06 08:33:38 -0600

    implement error handling

--- games/NATO-alphabet/main.py
+++ games/NATO-alphabet/main.py
@@ -6,7 +6,14 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 code_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 print(code_dict)
 
-word = input("Enter word.").upper()
-output = [code_dict[letter] for letter in word]
+def generate_phonetic():
+    word = input("Enter word.").upper()
+    try:
+        output = [code_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphavet, please.")
+        generate_phonetic()
+    else:
+        print(output)
 
-print(output)
+generate_phonetic()
