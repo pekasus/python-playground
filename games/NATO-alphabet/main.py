a8c76b079f365386d4c98ac4c501fe90961bfa76
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-07 10:35:33 -0600

    add text to speech to output

--- games/NATO-alphabet/main.py
+++ games/NATO-alphabet/main.py
@@ -1,13 +1,16 @@
 import pandas
-
+import pyttsx3
 
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 code_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 print(code_dict)
 
+engine = pyttsx3.init()
+
+
 def generate_phonetic():
-    word = input("Enter word.").upper()
+    word = input("Enter word: ").upper()
     try:
         output = [code_dict[letter] for letter in word]
     except KeyError:
@@ -15,5 +18,11 @@ def generate_phonetic():
         generate_phonetic()
     else:
         print(output)
+        for code in output:
+            engine.say(code)
+        engine.runAndWait()
+
+# engine.save_to_file('Hello World', 'test.mp3')
+# engine.runAndWait()
 
 generate_phonetic()
