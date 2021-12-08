ed331151a826a7cd440ad1826e0896c147778257
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-08 12:07:17 -0600

    working ISS emailer

--- science/iss-location/main.py
+++ science/iss-location/main.py
@@ -1,8 +1,9 @@
 import requests
 from datetime import datetime
+import smtplib
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 35.468491 # Your latitude
+MY_LON = -97.521263 # Your longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -11,12 +12,14 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
-
+if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LON-5 <= iss_longitude <= MY_LON+5:
+    iss_near = True
+else:
+    iss_near = False
 
 parameters = {
     "lat": MY_LAT,
-    "lng": MY_LONG,
+    "lng": MY_LON,
     "formatted": 0,
 }
 
@@ -26,12 +29,24 @@ data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
+time_now = datetime.utcnow().hour
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+def send_email(message):
+    my_gmail = "NyetWork001@gmail.com"
+    my_yahoo = "NyetWork001@yahoo.com"
 
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        with open("emailpass.txt", "r") as f:
+            password = f.read()
 
+        connection.login(my_gmail, password)
+        connection.sendmail(from_addr=my_gmail, to_addrs=my_yahoo,
+                            msg=f"Subject:ISS is near!\n\n{message}")
+
+# iss_near = True # DEBUG
+if iss_near:
+    if time_now < sunrise or time_now > sunset:
+        message = f"ISS is at {iss_latitude}, {iss_longitude}."
+        send_email(message)
 
