d7f307ec0533e1395f914449374de88c731b2dd0
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-08 11:21:30 -0600

    functioning kanye quotes

--- games/kanye-quotes/main.py
+++ games/kanye-quotes/main.py
@@ -1,11 +1,11 @@
 from tkinter import *
-
+import requests
 
 def get_quote():
-    pass
-    #Write your code here.
-
-
+    response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
+    quote = response.json()['quote']
+    canvas.itemconfig(quote_text, text=quote)
 
 window = Tk()
 window.title("Kanye Says...")
@@ -21,6 +21,6 @@ kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
-
+get_quote()
 
 window.mainloop()
