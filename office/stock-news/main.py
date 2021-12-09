36b25de4266235ac9d47a697be7ad3cc626e2fae
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-09 15:50:17 -0600

    fetch all news articles in stock-news

--- office/stock-news/main.py
+++ office/stock-news/main.py
@@ -1,3 +1,6 @@
+import requests
+from datetime import datetime, timedelta
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
@@ -7,9 +10,46 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 ## STEP 1: Use https://newsapi.org/docs/endpoints/everything
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
 
+with open("pass.txt", "r") as f:
+    alpha_key = f.read()
+
+with open("pass2.txt", "r") as f:
+    news_key = f.read()
+
+def get_news(company):
+    news_params = {
+        "q": COMPANY_NAME,
+        "apiKey": news_key
+    }
+    response = requests.get(NEWS_ENDPOINT, params=news_params)
+    data = response.json()['articles']
+    for article in data:
+        print(article)
+
+
+# https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo
+alpha_params = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": STOCK,
+    "apikey": alpha_key
+}
+
+response = requests.get(STOCK_ENDPOINT, params=alpha_params)
+data = response.json()['Time Series (Daily)']
+
+now = datetime.now()
+yesterday = now - timedelta(days=1)
+day_before = now - timedelta(days=2)
+
+yesterday = yesterday.strftime("%Y-%m-%d")
+day_before = day_before.strftime("%Y-%m-%d")
+yesterday_close = float(data[yesterday]['4. close'])
+day_before_close = float(data[day_before]['4. close'])
+diff_percent = (abs(yesterday_close - day_before_close)) / yesterday_close
+
+if diff_percent >= 0.005: # DEBUG change to 0.05 for production
+    get_news(COMPANY_NAME)
 
 
 ## STEP 2: Use https://newsapi.org/docs/endpoints/everything
