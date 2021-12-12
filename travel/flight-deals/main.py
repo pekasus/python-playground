493e85ec61045d795aa0c67f50b8028ad50a35aa
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-12 05:38:37 -0600

    update flight-deals

--- travel/flight-deals/main.py
+++ travel/flight-deals/main.py
@@ -1,1 +1,7 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from flight_data import FlightData
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+
+
+
