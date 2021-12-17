29949f8df8d4d4f79dfdd14f5c37dc28e2841e48
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-16 19:49:46 -0600

    working selenium to pull events from python.org

--- web/selenium-demo/main.py
+++ web/selenium-demo/main.py
@@ -9,7 +9,7 @@ driver = webdriver.Chrome(service=s)
 # price = driver.find_element(By.CLASS_NAME, "apexPriceToPay")
 # print(price.text)
 
-driver.get("https://www.python.org")
+# driver.get("https://www.python.org")
 # search_bar = driver.find_element(By.NAME, "q")
 # print(search_bar.get_attribute("placeholder"))
 #
@@ -21,6 +21,24 @@ driver.get("https://www.python.org")
 
 # bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
 # print(bug_link.text)
+
+
+driver.get("https://www.python.org")
+containers = driver.find_elements(By.CLASS_NAME, "shrubbery")
+items = containers[1].find_elements(By.TAG_NAME, "li")
+
+events = {}
+i = 0
+for item in items:
+    time_element = item.find_element(By.TAG_NAME, "time")
+    date = time_element.get_attribute('datetime').split('T')[0]
+    event = item.find_element(By.TAG_NAME, 'a').text
+    events[str(i)] = {"date": date, "name": event}
+    i += 1
+
+print(events)
+
+
 
 
 
